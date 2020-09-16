bffb863b3e7f114ef7f2768f2b856b34c4b2732c
eddo <eddojansen@gmail.com>
2020-09-16 23:54:33 +0000

    fixed gpu discovery

--- setenv.sh
+++ setenv.sh
@@ -1,12 +1,11 @@
 ##setenv.sh##
-#module load JAVA/jdk1.8.0_31 spark
 export MOUNT=/nfs
 export SPARK_HOME=$MOUNT/spark
 export PATH=$PATH:$SPARK_HOME/sbin:$SPARK_HOME/bin
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export SPARK_CONF_DIR=~/SparkConf
 mkdir -p $SPARK_CONF_DIR
 mkdir -p $MOUNT/sparkRapidsPlugin
+export SPARK_CONF_DIR=/nfs/spark/conf
 #
 export SPARK_RAPIDS_DIR=$MOUNT/sparkRapidsPlugin
 export CUDF_JAR_NAME="cudf-0.14-cuda10-1.jar"
@@ -19,8 +18,8 @@ export RAPIDS_PLUGIN_URL="https://repo1.maven.org/maven2/com/nvidia/rapids-4-spa
 
 #
 env=$SPARK_CONF_DIR/spark-env.sh
-echo "export SPARK_LOG_DIR=~/SparkLog" > $env
-echo "export SPARK_WORKER_DIR=~/SparkWorker" >> $env
+echo "export SPARK_LOG_DIR=/nfs/spark/log" > $env
+echo "export SPARK_WORKER_DIR=/nfs/spark/sparkworker" >> $env
 echo "export SLURM_MEM_PER_CPU=$SLURM_MEM_PER_CPU" >> $env
 echo 'export SPARK_WORKER_CORES=`nproc`' >> $env
 echo 'export SPARK_WORKER_MEMORY=$(( $SPARK_WORKER_CORES*$SLURM_MEM_PER_CPU ))M' >> $env
@@ -30,11 +29,15 @@ echo "export RAPIDS_JAR_NAME=$RAPIDS_JAR_NAME" >> $env
 echo "export SPARK_RAPIDS_DIR=$SPARK_RAPIDS_DIR" >> $env
 echo "export SPARK_CUDF_JAR=$SPARK_RAPIDS_DIR/$CUDF_JAR_NAME" >> $env
 echo "export SPARK_RAPIDS_PLUGIN_JAR=$SPARK_RAPIDS_DIR/$RAPIDS_JAR_NAME" >> $env
-echo 'export SPARK_WORKER_OPTS="'$WORKER_OPTS'"' >> $env
+echo "export SPARK_WORKER_OPTS='"$WORKER_OPTS"'" >> $env
 #
 echo "export SPARK_HOME=$SPARK_HOME" > ~/.bashrc
 echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
 echo "export SPARK_CONF_DIR=$SPARK_CONF_DIR" >> ~/.bashrc
+echo "export RAPIDS_JAR_NAME=$RAPIDS_JAR_NAME" >> ~/.bashrc
+echo "export SPARK_RAPIDS_DIR=$SPARK_RAPIDS_DIR" >> ~/.bashrc
+echo "export SPARK_CUDF_JAR=$SPARK_RAPIDS_DIR/$CUDF_JAR_NAME" >> ~/.bashrc
+echo "export SPARK_RAPIDS_PLUGIN_JAR=$SPARK_RAPIDS_DIR/$RAPIDS_JAR_NAME" >> ~/.bashrc
 
 scontrol show hostname $SLURM_JOB_NODELIST > $SPARK_CONF_DIR/slaves
 
